7a6c7e602e8185771faef21a1bb45f0b08b72034
liviator <h.f-g@hotmail.fr>
2022-06-10 10:00:20 +0200

    code cleaning

--- src/vues/Counter.vue
+++ src/vues/Counter.vue
@@ -15,7 +15,7 @@
 
 <script>
 import Web3 from "web3";
-import abi from "../assets/abi.json"
+import abi from "../assets/abi.json";
 const address_contract = "0x13A6DAadFABE9295eC96da1c626FB672C50F7a04";
 const provider = window.ethereum;
 const web3 = new Web3(provider);
@@ -32,7 +32,7 @@ export default {
         }
     },
     async mounted () {
-        this.count = await contract.methods.counter().call()
+        this.count = await contract.methods.counter().call();
     },
     methods: {
 
@@ -52,13 +52,13 @@ export default {
                 });
                 const response = await request.json();
                 if (request.status == 200) {
-                    this.message = "User is whitelisted, proceding to counter's modification"
-                    return response
+                    this.message = "User is whitelisted, proceding to counter's modification";
+                    return response;
 
                 } else {
-                    this.processing = false
-                    this.message = response.error
-                    this.fail = true
+                    this.processing = false;
+                    this.message = response.error;
+                    this.fail = true;
                     return -1
                 }
             } catch(e) {
@@ -70,7 +70,7 @@ export default {
             try {
             await provider.enable();
             const address = await web3.eth.getAccounts();
-            const token = await this.RetrieveToken(address)
+            const token = await this.RetrieveToken(address);
             if(token != -1) {
                 const result = await contract.methods.increment(token.token.r, token.token.s, token.token.v).send({from:address[0]}              ) 
                     if(result.status == true){
@@ -96,14 +96,14 @@ export default {
                 if(this.count >0) {
                     await provider.enable();
                     const address = await web3.eth.getAccounts();
-                    const token = await this.RetrieveToken(address)
+                    const token = await this.RetrieveToken(address);
                     if(token != -1) {
                        const result = await contract.methods.decrement(token.token.r, token.token.s, token.token.v).send({from:address[0]})
                             if(result.status == true){
                                 this.success = true;
                                 this.processing = false;
                                 this.message = "Counter Decremented";
-                                this.count = this.count - 1
+                                this.count = this.count - 1;
                             } else {
                                 this.fail=true;
                                 this.processing = false;
@@ -112,8 +112,9 @@ export default {
                          
                     } 
                 }   else {
-                        this.fail=true
-                        this.message = "Counter can't be negative"
+                        this.fail=true;
+                        this.success = false;
+                        this.message = "Counter can't be negative";
                     }
                 
 
@@ -137,14 +138,14 @@ export default {
     border-radius: 25px;
     padding-top: 20px;
     padding-bottom:20px;
-    top:150px;
+    top:200px;
     position: relative;
     align-items: center;
     justify-content: center;
 }
 .count_failure {
     background-color: rgba(242,0,0,0.37);
-    top:150px;
+    top:200px;
     border-radius: 25px;
     padding-top: 20px;
     padding-bottom:20px;
@@ -156,7 +157,7 @@ export default {
 .count_success {
     background-color: #18F518;
     border-radius: 25px;
-    top:150px;
+    top:200px;
     padding-top: 20px;
     padding-bottom:20px;
     position: relative;
